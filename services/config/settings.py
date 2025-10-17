146192341515fb600b582987d5040a1adc227f0a
Rauf Hamidy <raufhamidy10@gmail.com>
2025-10-17 10:34:52 +0800

    Fix Url 7

--- services/config/settings.py
+++ services/config/settings.py
@@ -25,8 +25,8 @@ class Settings(BaseSettings):
   TELEGRAM_BASE: int = logging.ERROR
 
   # API
-  #API_ENDPOINT: str = "https://leaddits-api-production.up.railway.app/"
-  API_ENDPOINT: str = "https://leaddits-api.railway.internal:8080/"
+  API_ENDPOINT: str = "https://leaddits-api-production.up.railway.app/"
+  #API_ENDPOINT: str = "https://leaddits-api.railway.internal:8080/"
 
   # Scheduler (UTC)
   LIST_RULES: bool = False
