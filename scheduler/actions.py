8aa0ad08b0f34dd491e654d8fef251824da4edd6
Rauf Hamidy <raufhamidy10@gmail.com>
2025-10-29 14:12:04 +0800

    Setup With Angles 3

--- scheduler/actions.py
+++ scheduler/actions.py
@@ -261,18 +261,19 @@ def do_load(logger):
   # --- Process alerts data to load-ready state ---
   to_load_data = []
   for item in alerts_data:
-    if data.get("score") > 50:
+    sub = item.get("submissions") or {}
+    data = sub.get("data") or {}
+    score = data.get("score")
+
+    if score => 50:
 
       sid = item.get("unique_key")
       relevance = item.get("relevance")
       agenda_type = (item.get("messageagendas") or {}).get("type")
 
-      sub = item.get("submissions") or {}
-      data = sub.get("data") or {}
       subreddit = sub.get("subreddit")
 
       title = data.get("title")
-      score = data.get("score")
       author = data.get("author")
       permalink = data.get("permalink")
       created_ts = data.get("created_utc")
