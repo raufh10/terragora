d455840b66a993ad8f811d4f1508390baa9d674b
root <root@localhost.localdomain>
2026-03-28 15:24:42 +0000

    Add typing 1

--- api/services/pg.py
+++ api/services/pg.py
@@ -7,7 +7,6 @@ def get_db_connection():
   return psycopg.connect(configs.conn_str.get_secret_value(), row_factory=dict_row)
 
 def fetch_relevant_posts(conn, query_embedding: list, limit: int = 5):
-
   query = """
     SELECT 
       id, 
@@ -17,6 +16,7 @@ def fetch_relevant_posts(conn, query_embedding: list, limit: int = 5):
       metadata,
       1 - (embedding <=> %s::vector) AS similarity
     FROM reddit_posts
+    WHERE is_active = true
     ORDER BY embedding <=> %s::vector
     LIMIT %s;
   """
@@ -24,4 +24,3 @@ def fetch_relevant_posts(conn, query_embedding: list, limit: int = 5):
   with conn.cursor() as cur:
     cur.execute(query, (query_embedding, query_embedding, limit))
     return cur.fetchall()
-
