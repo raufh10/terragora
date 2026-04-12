20d5736e3ec93db4f0f55638bc1322367310e894
raufh10 <raufhamidy10@gmail.com>
2026-04-12 04:54:02 +0000

    feat: add listen notify to pg and setup pipeline event to nats

--- internal/pkg/nats/models.go
+++ internal/pkg/nats/models.go
@@ -6,3 +6,15 @@ type ScraperEvent struct {
   Target    string `json:"target"`
   Timestamp string `json:"timestamp"`
 }
+
+type PipelineEvent struct {
+  ID         string                 `json:"id"`
+  RedditID   string                 `json:"reddit_id"`
+  Title      string                 `json:"title"`
+  Content    string                 `json:"content"`
+  URL        string                 `json:"url"`
+  PostedAt   string                 `json:"posted_at"`
+  ScrapedAt  string                 `json:"scraped_at"`
+  Metadata   map[string]interface{} `json:"metadata"`
+  IsActive   bool                   `json:"is_active"`
+}
